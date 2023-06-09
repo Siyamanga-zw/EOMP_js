297e74dbde3adf77c9cd486277d08e99ae3ae7b4
siyamanga22 <zwenisiyamanga@gmail.com>
2023-06-09 16:25:45 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,27 @@
+let home = document.querySelector('#home');
+let homeContent=[
+    {
+        h1:'GirlHeaven <br> <span style="margin-left:90px;font-size:60px;">beauty bar</span>',
+        h3:'<span style="font-weight: bold;font-size:40px;">Let us transport you to GirlHeaven.</span><br>Get a wonderful and memorable experience from the minute you walk in.',
+        btn:'ShopNow',
+        img:'https://i.postimg.cc/DyQKSGzJ/top-of-background.jpg',
+
+}
+]
+
+homeContent.forEach((content)=>{
+    home.innerHTML +=`
+<div id = "landing" class="container text-center">
+  <div class="row align-items-center">
+    <div class="col">
+      <h1 class="card-title">${content.h1}</h1><br>
+      <h3 class="card-text">${content.h3}</h3>
+      <button class="card-text"><a href="../html/products.html">${content.btn}</a></button>
+    </div>
+    <div class="col">
+      <img src="${content.img}" alt="logo">
+    </div>
+  </div>
+</div>`
+})
+
