3c14324cedfd0586aefec1fb96280f114cf1a17c
siyamanga <zwenisiyamanga@gmail.com>
2023-06-15 07:21:59 +0200

    done

--- script/checkout.js
+++ script/checkout.js
@@ -22,11 +22,18 @@ function displayContent() {
 function calculateTotalPrice() {
   let totalPrice = 0;
   cart.forEach((item) => {
-    // Extract the price from each item and add it to totalPrice
-    totalPrice += parseFloat(item.price);
+    // Extract the price from each item and remove "R"
+    const priceWithoutR = item.price.replace("R", "");
+    
+    // Remove the decimal point
+    // const priceWithoutDecimal = priceWithoutR.replace(".", "");
+    
+    // Convert the modified price to a number and add it to totalPrice
+    totalPrice += parseFloat(priceWithoutR);
   });
 
   total.innerHTML = `Total Amount Due: ${totalPrice.toFixed(2)}`;
 }
 
 displayContent();
+
